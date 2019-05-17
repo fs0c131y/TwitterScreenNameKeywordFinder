5ffadf7d4687e10d7896add4bb25397f8d289a6d
Rahul Lakhanpal <rahulkp220@users.noreply.github.com>
2019-05-18 00:16:30 +0530

    Python styling updates + enhancements (#1)
    * Updates for unified imports
    
    * Convert tabs to spaces
    
    * Update get_followers_id to use list comprehensions
    
    * Use enumerate

--- poc.py
+++ poc.py
@@ -2,41 +2,53 @@ import sys
 
 from twarc import Twarc
 
-from secrets import access_secret
-from secrets import access_token
-from secrets import consumer_key
-from secrets import consumer_secret
+from secrets import (
+    access_secret,
+    access_token,
+    consumer_key,
+    consumer_secret
+)
 
 
 def get_screen_name(twarc, screen_name, follower_ids, keyword):
-	counter = 0
-	with open(screen_name + '_' + keyword + '.txt', 'a') as f:
-		for user in twarc.user_lookup(ids=follower_ids):
-			name = user["name"]
-			if keyword in name:
-				counter = counter + 1
-				f.write('[+] ' + str(counter) + ': screen_name = ' + user['screen_name'] + ', name = ' + name + '\n')
+    """
+    Get the twitter screen names
+
+    :param twarc:
+    :param screen_name:
+    :param follower_ids:
+    :param keyword:
+    :return:
+    """
+    with open(screen_name + '_' + keyword + '.txt', 'a') as f:
+        for counter, user in enumerate(twarc.user_lookup(ids=follower_ids), 1):
+            name = user["name"]
+            if keyword in name:
+                f.write('[+] ' + str(counter) + ': screen_name = ' + user['screen_name'] + ', name = ' + name + '\n')
 
 
 def get_followers_id(twarc, screen_name):
-	follower_ids = []
-	for follower_id in twarc.follower_ids(screen_name):
-		follower_ids.append(follower_id)
+    """
+    Returns the follower IDs
 
-	return follower_ids
+    :param twarc:
+    :param screen_name:
+    :return:
+    """
+    return [follower_id for follower_id in twarc.follower_ids(screen_name)]
 
 
 def main():
-	if len(sys.argv) > 2:
-		screen_name = sys.argv[1]
-		keyword = sys.argv[2]
+    if len(sys.argv) > 2:
+        screen_name = sys.argv[1]
+        keyword = sys.argv[2]
 
-		t = Twarc(consumer_key, consumer_secret, access_token, access_secret)
-		follower_ids = get_followers_id(t, screen_name)
-		get_screen_name(t, screen_name, follower_ids, keyword)
-	else:
-		print('Usage: python poc.py fs0c131y chowkidar')
+        t = Twarc(consumer_key, consumer_secret, access_token, access_secret)
+        follower_ids = get_followers_id(t, screen_name)
+        get_screen_name(t, screen_name, follower_ids, keyword)
+    else:
+        print('Usage: python poc.py fs0c131y chowkidar')
 
 
 if __name__ == '__main__':
-	main()
+    main()
